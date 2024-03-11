a9bc5a434e92157da5548a8987e60ba7534fe2b2
ddealsan <ddealsan@myuax.com>
2024-03-11 13:20:30 +0100

    continuamos añadiendo clases

--- src/main/java/gestion_habitats/Habitat.java
+++ src/main/java/gestion_habitats/Habitat.java
@@ -3,10 +3,12 @@ package gestion_habitats;
 public abstract class Habitat {
     private String nombre;
     private double tamaño;
+    private String ubicacion;
 
-    public Habitat(String nombre, double tamaño) {
+    public Habitat(String nombre, double tamaño, String ubicacion) {
         this.nombre = nombre;
         this.tamaño = tamaño;
+        this.ubicacion = ubicacion;
     }
 
     public String getNombre() {
@@ -17,6 +19,9 @@ public abstract class Habitat {
         return tamaño;
     }
 
-    public abstract String getTipo();
+    public String getUbicacion() {
+        return ubicacion;
+    }
 
+    public abstract String getTipo();
 }

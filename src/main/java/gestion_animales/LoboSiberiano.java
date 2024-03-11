87c3f2f39425ed8a9d1a03fa10b0082288388562
ddealsan <ddealsan@myuax.com>
2024-03-11 18:41:55 +0100

    continuamos añadiendo clases

--- src/main/java/gestion_animales/LoboSiberiano.java
+++ src/main/java/gestion_animales/LoboSiberiano.java
@@ -1,4 +1,39 @@
 package gestion_animales;
 
-public class LoboSiberiano {
+public class LoboSiberiano extends Terrestre {
+
+    private String colorPelaje;
+    private boolean esManada;
+
+    public LoboSiberiano(String nombre, String colorPelaje, int edad, boolean esManada) {
+        super(nombre, "lobo siberiano", edad, 4, "pelaje grueso");
+        this.colorPelaje = colorPelaje;
+        this.esManada = esManada;
+    }
+
+    public String getColorPelaje() {
+        return colorPelaje;
+    }
+
+    public void setColorPelaje(String colorPelaje) {
+        this.colorPelaje = colorPelaje;
+    }
+
+    public boolean isEsManada() {
+        return esManada;
+    }
+
+    public void setEsManada(boolean esManada) {
+        this.esManada = esManada;
+    }
+
+    @Override
+    public void reproducirse() {
+        System.out.println("El lobo siberiano " + nombre + " da a luz a cachorros.");
+    }
+
+    public void aullar() {
+        System.out.println("El lobo siberiano " + nombre + " está aullando.");
+    }
+
 }
